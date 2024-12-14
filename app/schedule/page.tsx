68be147bd816c4dfcb5c19d70349920a7b74287b
Phil Jin Sang <phil@Philui-MacBookPro.local>
2024-12-15 08:49:31 +0900

    configured names in schedule

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -10,7 +10,10 @@ import EnterButton from "@/components/Button/Button";
 import AddRoom from "@/components/addroom";
 import VariousRoom from "@/components/VariousRoom";
 
-const ToastUI = dynamic(() => import("@/components/ToastUI/ToastUI"), {
+const StudentToastUI = dynamic(() => import("@/components/ToastUI/student_toastui"), {
+  ssr: false,
+});
+const TeacherToastUI = dynamic(() => import("@/components/ToastUI/teacher_toastui"), {
   ssr: false,
 });
 
@@ -100,7 +103,7 @@ const SchedulePage = () => {
       </div>
       <div className="flex-1 flex justify-center items-center max-w-full max-h-full overflow-auto">
         <div className="bg-white w-[95%] h-[90%] max-w-full m-5 p-5 rounded-lg shadow-lg overflow-hidden">
-          <ToastUI data={classes} />
+          {type === "student" ? <StudentToastUI data={classes} /> : <TeacherToastUI data={classes} />}
         </div>
       </div>
       {/* AddRoom 모달 */}
